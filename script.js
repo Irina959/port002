e9ee5b3a8cab5a03beb625ae84e5b1c1306c4b84
Irina Maevskaya <84159131+Irina959@users.noreply.github.com>
2022-07-01 18:07:12 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+// $(document).ready(function() {
+//     $(`.header__burger`).click(function(event) {
+//         $(`.header__burger,.header__menu`).toggleClass('active');
+//     });
+// });
+
+
+// (() => {
+//     window.onload = () => {
+//       let header__burger = document.querySelector('.header__burger');
+//       let header__menu = document.querySelector('.header__menu');
+//       let body = document.querySelector('body');
+
+//       header__burger.addEventListener('click', (e)=> {
+//         header__burger.classList.toggle('active');
+//         header__menu.classList.toggle('active');
+//         body.classList.toggle('lock');
+//       })
+//     }
+//   })();
+
+
+let header__burger = document.querySelector('.header__burger');
+let header_menu = document.querySelector('.header__menu');
+let back = document.querySelector('body');
+let header__list = document.querySelector('.header__list');
+
+header__burger.onclick = function(){
+    header__burger.classList.toggle('active');
+    header_menu.classList.toggle('active');
+    back.classList.toggle('lock');
+}
+
+header__list.onclick = function () {
+    header__list.classList.remove('active');
+    back.classList.toggle('lock');
+}
